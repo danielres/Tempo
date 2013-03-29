daa8f20b91a8d0d4a7fa4fad21bf72f2ef40c483
Daniel Reszka <dannn.r@gmail.com>
2013-03-29 17:09:43 +0100

    Added datamapper models : Activity, Category and Fact

--- app.rb
+++ app.rb
@@ -5,7 +5,34 @@ DataMapper.setup(:default, "sqlite://#{Dir.pwd}/db/hamster.db")
 
 class Tag
   include DataMapper::Resource
-  property :id,   Serial
-  property :name, String
+  property :id,             Serial
+  property :name,           String
 end
 
+class Activity
+  include DataMapper::Resource
+  property :id,             Serial
+  property :name,           String
+  property :work,           Integer
+  property :activity_order, Integer
+  property :deleted,        Integer
+  property :category_id,    Integer
+  property :search_name,    String
+end
+
+class Category
+  include DataMapper::Resource
+  property :id,             Serial
+  property :name,           String
+  property :color_code,     String
+  property :category_order, Integer
+  property :search_name,    String
+end
+
+class Fact
+  include DataMapper::Resource
+  property :id,             Serial
+  property :start_time,     DateTime
+  property :end_time,       DateTime
+  property :description,    String
+end
