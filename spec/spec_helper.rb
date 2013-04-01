7f604e7f399549c2e2dede5b89ba7c5d4b6399eb
Daniel Reszka <dannn.r@gmail.com>
2013-04-01 21:37:09 +0200

    spec_helper: capybara setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,11 @@
+require 'capybara'
+require 'capybara/dsl'
+require 'capybara/rspec'
 require_relative '../app'
+Dir.glob("spec/acceptance/steps/*steps.rb") { |f| load f, true }
+
+
+def app
+  Sinatra::Application
+end
+Capybara.app = app
