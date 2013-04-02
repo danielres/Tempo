8c6571fa2da6b9e2329d9604c830f100b2d8667f
Daniel Reszka <dannn.r@gmail.com>
2013-04-02 21:26:18 +0200

    moved all turnip stuff from spec_helper to turnip_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,1 @@
-require 'capybara'
-require 'capybara/dsl'
-require 'capybara/rspec'
 require_relative '../app'
-Dir.glob("spec/acceptance/steps/*steps.rb") { |f| load f, true }
-
-
-def app
-  Sinatra::Application
-end
-Capybara.app = app
-
-def extract_words_from comma_separated_words
-  comma_separated_words.split(',').map(&:strip)
-end
---
+++ spec/turnip_helper.rb
@@ -0,0 +1,16 @@
+require_relative '../app'
+
+require 'capybara'
+require 'capybara/dsl'
+require 'capybara/rspec'
+Dir.glob("spec/acceptance/steps/*steps.rb") { |f| load f, true }
+
+
+def app
+  Sinatra::Application
+end
+Capybara.app = app
+
+def extract_words_from comma_separated_words
+  comma_separated_words.split(',').map(&:strip)
+end
