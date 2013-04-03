21333fc08244284cf55fbc819916ad4f190c76aa
Daniel Reszka <dannn.r@gmail.com>
2013-04-03 21:11:28 +0200

    implemented Timesheet#facts

--- models/timesheet.rb
+++ models/timesheet.rb
@@ -1,11 +1,13 @@
 class Timesheet
-  def initialize activity, month
-    @activity = activity
-    @month    = month
+  def initialize activity, requested_month
+    @activity         = activity
+    @requested_month  = requested_month
   end
 
   def facts
     @activity.facts
+             .select{ |f| f.start_time.year  == @requested_month.year  }
+             .select{ |f| f.start_time.month == @requested_month.month }
   end
 
 end
--- spec/models/timesheet_spec.rb
+++ spec/models/timesheet_spec.rb
@@ -2,20 +2,27 @@ require_relative '../spec_helper'
 
 describe Timesheet do
 
-  let( :activity   ) { Activity.new                  }
-  let( :month      ) { DateTime.new 2013, 01         }
-  let( :fact1      ) { Fact.new                      }
-  let( :fact2      ) { Fact.new                      }
-  let( :facts      ) { [ fact1, fact2 ]              }
-  let( :timesheet  ) { Timesheet.new activity, month }
 
-  it "creates a new timesheet given an activity and a month" do
-    timesheet = Timesheet.new activity, month
+  describe '#new' do
+    let( :timesheet       ) { Timesheet.new activity, requested_month }
+    let( :activity        ) { Activity.new                            }
+    let( :requested_month ) { DateTime.new                            }
+    it "creates a new timesheet given an activity and a month" do
+      timesheet = Timesheet.new activity, requested_month
+    end
   end
 
-  it "gets its facts from the activity" do
-    activity.stub facts: facts
-    timesheet.facts.should == facts
+  describe '#facts' do
+    let( :timesheet       ) { Timesheet.new activity, requested_month                       }
+    let( :activity        ) { Activity.new facts: [ before_fact, fact1, fact2, after_fact ] }
+    let( :before_fact     ) { Fact.new start_time: requested_month.prev_month               }
+    let( :fact1           ) { Fact.new start_time: requested_month                          }
+    let( :fact2           ) { Fact.new start_time: requested_month.next_day                 }
+    let( :after_fact      ) { Fact.new start_time: requested_month.next_month               }
+    let( :requested_month ) { DateTime.new 2013, 01                                         }
+    it "returns only facts included in the requested month" do
+      timesheet.facts.should =~ [ fact1, fact2 ]
+    end
   end
 
 end
