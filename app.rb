e628aa6778207e5f275dcd5e614051cae7d6e8c4
Daniel Reszka <dannn.r@gmail.com>
2013-04-07 23:06:03 +0200

    refactoring: using TimesheetExhibit and FactExhibit

--- app.rb
+++ app.rb
@@ -18,11 +18,10 @@ get '/activity/:id' do
   text = ""
   text << "<h1>Timesheets for #{activity.name}</h1>"
   activity_months(activity).each do |month|
-   text << exhibit_timesheet( Timesheet.new( activity, month ) )
+   text << exhibit( Timesheet.new( activity, month ) )
   end
   text
 end
-
 
 private
 
@@ -46,36 +45,53 @@ private
     activity.months
   end
 
-  def exhibit_timesheet timesheet
-    html = ''
-    timesheet.facts.each {|fact| html << exhibit_fact(fact) }
-    "<div data-purpose='timesheet'>
-     <h2>Timesheet for #{timesheet.year}-#{timesheet.month}</h2>
-     #{html}
-       <div class='time_spent'>
-       <span data-purpose='total_hours_count'>#{timesheet.total_hours_count} hours</span>
-       <span data-purpose='total_hours_count'>(#{timesheet.total_minutes_count} minutes)</span>
-       </div>
-     </div>"
+  def exhibit object
+    case object
+      when Timesheet then TimesheetExhibit.new(object)
+      when Fact      then      FactExhibit.new(object)
+    end.to_html
   end
 
-  def exhibit_fact fact
-    html = ''
-    html << "<span data-purpose='date_day'>
-              #{fact.start_time.strftime("%a %d")}
-             </span>"
-    html << "<span data-purpose='minutes_count'>
-              #{fact.duration}
-             </span>"
-    html << "<span data-purpose='start_time'>
-              #{fact.start_time.hour}:#{fact.start_time.minute}
-             </span>"
-    html << '-'
-    html << "<span data-purpose='end_time'>
-              #{fact.end_time.hour}:#{fact.end_time.minute}
-             </span>"
-    html << "<span data-purpose='description'>
-              #{fact.description}
-             </span>"
-    "<div data-purpose='fact'>#{html}</div>"
+  class TimesheetExhibit
+    def initialize timesheet
+      @timesheet = timesheet
+    end
+    def to_html
+      html = ''
+      @timesheet.facts.each {|fact| html << exhibit(fact) }
+      "<div data-purpose='timesheet'>
+       <h2>Timesheet for #{@timesheet.year}-#{@timesheet.month}</h2>
+       #{html}
+         <div class='time_spent'>
+         <span data-purpose='total_hours_count'>#{@timesheet.total_hours_count} hours</span>
+         <span data-purpose='total_hours_count'>(#{@timesheet.total_minutes_count} minutes)</span>
+         </div>
+       </div>"
+    end
+  end
+
+  class FactExhibit
+    def initialize fact
+      @fact = fact
+    end
+    def to_html
+      html = ''
+      html << "<span data-purpose='date_day'>
+                #{@fact.start_time.strftime("%a %d")}
+               </span>"
+      html << "<span data-purpose='minutes_count'>
+                #{@fact.duration}
+               </span>"
+      html << "<span data-purpose='start_time'>
+                #{@fact.start_time.hour}:#{@fact.start_time.minute}
+               </span>"
+      html << '-'
+      html << "<span data-purpose='end_time'>
+                #{@fact.end_time.hour}:#{@fact.end_time.minute}
+               </span>"
+      html << "<span data-purpose='description'>
+                #{@fact.description}
+               </span>"
+      "<div data-purpose='fact'>#{html}</div>"
+    end
   end
