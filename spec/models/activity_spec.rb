d5b0e2dd7997accee2edab55f1868b08257a40d9
Daniel Reszka <dannn.r@gmail.com>
2013-09-17 23:00:21 +0200

    Clean activity_spec, add '-I models' to file .rspec

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,49 +1,58 @@
-require_relative '../spec_helper'
-require_relative '../../models/activity'
+require 'spec_helper'
+require 'activity'
+
+  # Can't figure how to run this spec in isolation with datamapper...
+  # So we have to also require these:
+    require 'fact'
+    require 'category'
 
 
 describe Activity do
 
-  let( :activity ){ Activity.new }
-
-  describe '#new' do
-    it "has proper attributes" do
-      attributes  = %w( id name work activity_order deleted category_id search_name )
-      attributes.each{ |attribute| activity.should respond_to attribute }
-    end
+  describe 'new activity' do
+    let( :activity ){ Activity.new }
+    it( "supports attribute 'id'"             ){ expect( activity ).to respond_to :id             }
+    it( "supports attribute 'name'"           ){ expect( activity ).to respond_to :name           }
+    it( "supports attribute 'work'"           ){ expect( activity ).to respond_to :work           }
+    it( "supports attribute 'activity_order'" ){ expect( activity ).to respond_to :activity_order }
+    it( "supports attribute 'deleted'"        ){ expect( activity ).to respond_to :deleted        }
+    it( "supports attribute 'category_id'"    ){ expect( activity ).to respond_to :category_id    }
+    it( "supports attribute 'search_name'"    ){ expect( activity ).to respond_to :search_name    }
   end
 
-  describe '#category' do
-    let( :category ){ double.as_null_object }
-    let( :activity ){ Activity.new.tap{ |a| a.category = category } }
-    it "belongs to a category" do
+  describe 'having a category' do
+    let( :category ){ double.as_null_object        }
+    let( :activity ){ Activity.new                 }
+    before( :each  ){ activity.category = category }
+    it "supports having a category" do
       activity.category.should == category
     end
   end
 
-  describe '#facts' do
-    let( :fact1               ){ double(:fact1).as_null_object }
-    let( :fact2               ){ double(:fact2).as_null_object }
-    let( :facts               ){ [ fact1, fact2 ] }
-    let( :activity_with_facts ){ Activity.new.tap{ |a| a.facts = facts } }
-    it "has many facts" do
-      expect( activity_with_facts.facts ).to match_array facts
+  describe 'having facts' do
+    let( :fact1               ){ double(:fact1).as_null_object                      }
+    let( :fact2               ){ double(:fact2).as_null_object                      }
+    let( :activity_with_facts ){ Activity.new.tap{ |a| a.facts = [ fact1, fact2 ] } }
+    it "supports having many facts" do
+      expect( activity_with_facts.facts ).to match_array [ fact1, fact2 ]
     end
   end
 
-  describe '#months' do
-    it "returns a list of months when the activity occurs" do
-      month1  = DateTime.new( 2012, 1 )
-      month2  = DateTime.new( 2013, 1 )
-      month3  = DateTime.new( 2013, 2 )
-      ( fact1 = double.as_null_object ).stub start_time: month1
-      ( fact2 = double.as_null_object ).stub start_time: month2
-      ( fact3 = double.as_null_object ).stub start_time: month2
-      ( fact4 = double.as_null_object ).stub start_time: month3
-      facts   = [ fact1, fact2, fact3, fact4 ]
-      activity_with_facts = activity.tap{ |c| c.facts = facts }
-      activity_with_facts.months.should =~ [ month1, month2, month3 ]
+  describe 'displaying months' do
+    let( :activity ){ Activity.new }
+    let( :month1 ){ DateTime.new( 2012, 1 ) }
+    let( :month2 ){ DateTime.new( 2013, 1 ) }
+    let( :month3 ){ DateTime.new( 2013, 2 ) }
+    let( :fact1  ){ double( start_time: month1 ).as_null_object }
+    let( :fact2  ){ double( start_time: month2 ).as_null_object }
+    let( :fact3  ){ double( start_time: month2 ).as_null_object }
+    let( :fact4  ){ double( start_time: month3 ).as_null_object }
+    let( :activity_with_facts ) do
+      activity.facts = [ fact1, fact2, fact3, fact4 ]
+      activity
+    end
+    it 'lists months when the activity took place' do
+      expect( activity_with_facts.months ).to match_array [ month1, month2, month3 ]
     end
   end
-
 end
