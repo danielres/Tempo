8bdb0d409b0d6d5b4fa5d1a6d35b67c086c75dcb
Daniel Reszka <dannn.r@gmail.com>
2013-09-17 23:39:26 +0200

    Clean fact_spec.rb

--- spec/models/fact_spec.rb
+++ spec/models/fact_spec.rb
@@ -1,33 +1,40 @@
-require_relative '../spec_helper'
-require_relative '../../models/fact'
+require 'spec_helper'
+require 'fact'
+
+  # Can't figure how to run this spec in isolation with datamapper...
+  # So we have to also require these:
+    require 'activity'
 
 
 describe Fact do
 
-  let( :fact ){ Fact.new }
+  describe 'new fact' do
+    let( :fact ){ Fact.new }
+    it( "supports attribute 'id'"          ){ expect( fact ).to respond_to :id }
+    it( "supports attribute 'start_time'"  ){ expect( fact ).to respond_to :id }
+    it( "supports attribute 'end_time'"    ){ expect( fact ).to respond_to :id }
+    it( "supports attribute 'description'" ){ expect( fact ).to respond_to :id }
+  end
 
-  describe '#new' do
-    it "has proper attributes" do
-      attributes  = %w( id start_time end_time description )
-      attributes.each{ |attribute| fact.should respond_to attribute }
+  describe 'having an activity' do
+    let( :activity ){ double.as_null_object    }
+    let( :fact     ){ Fact.new                 }
+    before( :each  ){ fact.activity = activity }
+    it 'has an activity' do
+      expect( fact.activity ).to be activity
     end
   end
 
-  describe '#activity' do
-    let( :activity ){ double.as_null_object }
-    let( :fact ){ Fact.new.tap{ |f| f.activity = activity } }
-    it "belongs to an activity" do
-      fact.activity.should == activity
+  describe 'having a duration' do
+    let( :fact   ){ Fact.new }
+    before(:each ) do
+      start_time = Time.new( 2013,1,1,1,0 )
+      duration   = 10 * 60 # 10 minutes
+      end_time   = start_time + duration
+      fact.stub( start_time: start_time, end_time: end_time  )
     end
-  end
-
-  describe '#duration' do
-    start_time = Time.new 2013,1,1,1,0
-    duration   = 10*60 # 10 minutes
-    end_time   = start_time + duration
-    before { fact.stub( start_time: start_time, end_time: end_time  ) }
-    it "returns its duration in minutes" do
-      fact.duration.should == 10
+    it 'returns its duration in minutes' do
+      expect( fact.duration ).to eq 10
     end
   end
 
