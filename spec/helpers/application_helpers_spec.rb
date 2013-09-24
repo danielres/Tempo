9db9c1f3d72f5515c6dab863ec4c0d2d9a9339f6
Daniel Reszka <dannn.r@gmail.com>
2013-09-24 08:55:31 +0200

    Cleanup exhibit helper spec in application_helpers_spec.rb

--- spec/helpers/application_helpers_spec.rb
+++ spec/helpers/application_helpers_spec.rb
@@ -6,20 +6,20 @@ include ApplicationHelpers
 
 describe ApplicationHelpers do
   describe 'exhibit helper' do
-    context 'when given a Timesheet, a context and a format' do
-      let( :timesheet        ){ stub_const( 'Timesheet', double.as_null_object) }
+    context 'when given a context and a format' do
       let( :context          ){ double( 'context', settings: context_settings ).as_null_object }
       let( :context_settings ){ double( 'settings', root: '.' )     }
-      it 'returns a TimesheetExhibit object' do
-        exhibit( timesheet, format: 'default', context: context ).to be_kind_of TimesheetExhibit
+      context 'with a Timesheet object' do
+        let( :timesheet ){ stub_const( 'Timesheet', double.as_null_object) }
+        it 'returns a TimesheetExhibit object' do
+          exhibit( timesheet, format: 'default', context: context ).to be_kind_of TimesheetExhibit
+        end
       end
-    end
-    context 'when given a Fact, a coontext and a format' do
-      let( :fact             ){ stub_const( 'Fact', double.as_null_object) }
-      let( :context          ){ double( 'context', settings: context_settings ).as_null_object }
-      let( :context_settings ){ double( 'settings', root: '.' )     }
-      it 'returns a FactExhibit object' do
-        exhibit( fact, format: 'default', context: context ).to be_kind_of FactExhibit
+      context 'with a Fact object' do
+        let( :fact ){ stub_const( 'Fact', double.as_null_object) }
+        it 'returns a FactExhibit object' do
+          exhibit( fact     , format: 'default', context: context ).to be_kind_of FactExhibit
+        end
       end
     end
   end
